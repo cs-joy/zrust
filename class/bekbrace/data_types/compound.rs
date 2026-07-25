92825f01a9b023f07712fd7e48f8f4c336a569ce
Md Zahangir Alam Joy <mdzahangiralam1001@gmail.com>
2026-07-25 23:56:44 +0600

    string and string slice

--- class/bekbrace/data_types/compound.rs
+++ class/bekbrace/data_types/compound.rs
@@ -47,4 +47,31 @@ fn main() {
 
     let book_slices: &[&String] = &[&"Science".to_string(), &"Math".to_string(), &"English".to_string()];
     println!("book slices: {:?}", book_slices);
+
+    // Strings vs String Slices (&str)
+    // String: are growable which means expandable, you can increase or decrease them if we want // enhance they are mutable so we can push or delete them from a certain variable. They are own string type that means they are not borrowed
+    // String [ growable, mutable, owned string type]
+    let mut stone_cold: String = String::from("Hello, "); // store on the Heap
+    stone_cold.push_str("World");
+    println!("Stone Cold Says: {}", stone_cold);
+
+    // String Slice (&str): you can modify anything and not growable
+    // String Slice [immutable, reference]
+    // String Slices are used to reference strings literals or substrings (string objects with out needing to copy or own the data),, the is good for memory efficiency, because you don't have to copy the same variable. 
+    // So these string slices are used when you want to work with string data without taking ownership of it.
+    // The also have specific size and no number of bytes to the stack.
+    // So the stack remembers that very well and reacts very quickly in contrast to the heap which is expandable and you know dynamic.
+    // It goes at the runtime in the background and can grow bigger and bigger and bigger and can be slower and slower.
+    
+    // So that's the main difference between the heap and the stack. So, Stack is quicker, the heap is slower.
+    // But the stack can't have immutable data types while the heap can have dynamic mutable data types.
+    let string: String = String::from("Hello, Rust!");
+    //let slice: &str = &string;
+    let slice: &str = &string[0..5];
+    println!("slice value: {}", slice);
 }
+
+// this following function wil generate an error called "not found in this scope, since this is outside of the main function"
+// fn print_slice() {
+//     println!("SLICE: {}", slice);
+// }
