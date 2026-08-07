0252f5e9419907d253316c6b7ba13b4e8ccf6b7d
Md Zahangir Alam Joy <mdzahangiralam1001@gmail.com>
2026-08-07 10:09:26 +0600

    about comment

---
+++ class/bekbrace/_8comments/src/main.rs
@@ -0,0 +1,20 @@
+// Comments
+// Comments are very important part of coding. Especially if you work with two
+// or three team members, it's important for all of you to understand what 
+// everybodu else is doing. It's important to describe the myterious parts of the 
+// difficult parts. for someone else to read your code. he needs or she needs to
+// understand exactly what the code block is doing. And you can achive that
+// through commenting.
+
+// this is a single line comment
+/* 
+this
+ is
+ a
+ multi line or block comment
+*/
+
+
+fn main() {
+    println!("Hello, world!");
+}
