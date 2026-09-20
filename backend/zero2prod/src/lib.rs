19599039bde3a01a5f58e01d8aa05d1609c29f4e
Md Zahangir Alam Joy <mdzahangiralam1001@gmail.com>
2026-09-20 17:20:34 +0600

    robust setup!

--- backend/zero2prod/src/lib.rs
+++ backend/zero2prod/src/lib.rs
@@ -1,6 +1,7 @@
 // crates
 use actix_web::{web, App, HttpResponse, HttpServer};
 use actix_web::dev::Server;
+use std::net::TcpListener;
 
 // request handler
 async fn health_check() -> HttpResponse {
@@ -10,12 +11,49 @@ async fn health_check() -> HttpResponse {
 // Notice the different signature!
 // We return `Server` on the happy path and we dropped the `async` keyword
 // We have no `.await` call, so it is not needed anymore
-pub fn run() -> Result<Server, std::io::Error> {
+// pub fn run() -> Result<Server, std::io::Error> {
+//     let server = HttpServer::new(|| {
+//         App::new()
+//             .route("/health_check", web::get().to(health_check))
+//         })
+//         .bind("127.0.0.1:8000")?
+//         .run();
+//     // No .await here!
+//     Ok(server)
+// }
+
+// 3.5.1: Polishing -> improve
+// pub fn run(address: &str) -> Result<Server, std::io::Error> {
+//     let server = HttpServer::new(|| {
+//         App::new()
+//             .route("/health_check", web::get().to(health_check))
+//     })
+//         .bind(address)?
+//         .run();
+//     // No .await here!
+//     Ok(server)
+// }
+
+// ...
+// Failed to execute request.: reqwest::Error { kind: Request, url: Url { scheme: "http",
+// cannot_be_a_base: false, username: "", password: None, host: Some(Ipv4(127.0.0.1)),
+// port: Some(8000), path: "/health_check", query: None, fragment: None },
+// source: hyper::Error(Connect, ConnectError("tcp connect error", Os { code: 111,
+// kind: ConnectionRefused, message: "Connection refused" })) }
+// note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+
+// the problem is currently our HTTP client is still calling `127.0.0.1:8000` and we really
+// don't know what to put there now: the application port is determined at runtime, we can't
+// hard code it there.
+// We need, somehow, to find out what port the OS has gifted our application and return it from
+// `spawn_app`.
+// There are a few ways to go about it - we will use a `std::net::TcpListener`
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-        })
-        .bind("127.0.0.1:8000")?
+    })
+        .listen(listener)?
         .run();
     // No .await here!
     Ok(server)
--- backend/zero2prod/tests/health_check.rs
+++ backend/zero2prod/tests/health_check.rs
@@ -1,21 +1,27 @@
+use std::net::TcpListener;
+
 // `tokio::test` is the testing equivalent of `tokio::main`.
 // It also spares you from having to specify the `#[test]` attribute.
 //
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name f the test file)
-# [tokio::test]                     // macro!
+#[tokio::test]                     // macro!
 async fn health_check_works() {
     // Arrange
     // spawn_app().await.expect("Failed to spawn our app");
-    spawn_app(); // no .await, no .expect
+    // spawn_app(); // no .await, no .expect
 
+    // v2: now we leverage the application address in our test to point out our `reqwest::Client`
+    // Arrange
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        // Use the returned application address
+        .get(&format!("http://{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -35,10 +41,39 @@ async fn health_check_works() {
 // We are also running tests, so it is not worth it to propagate errors:
 // if we fail to perform the required setup we can just panic and
 // crash all the things
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to find address");
+// fn spawn_app() {
+//     let server = zero2prod::run().expect("Failed to find address");
+//     // Launch the server as a background task
+//     // tokio::spawn returns a handle to the spawned future,
+//     // but we have to use for it here, hence the non-binding let
+//     let _ = tokio::spawn(server);
+// }
+
+// 3.5.1: Polishing
+// fn spawn_app() {
+//     let server = zero2prod::run("127.0.0.1:0").expect("Failed to find address");
+//     // Launch the server as a background task
+//     // tokio::spawn returns a handle to the spawned future,
+//     // but we have to use for it here, hence the non-binding let
+//     let _ = tokio::spawn(server);
+// }
+
+// v2
+fn spawn_app() -> String {
+    // bind port with TcpListener
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+
+    // Hand over to the HttpServer
+    let server = zero2prod::run(listener).expect("Failed to find address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have to use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+
+    // We return the appliation address to the called!
+    format!("127.0.0.1:{}", port)
 }
